1d101e1def9230dbc73896501fdabf58ec11f83f
okengwu <okengwuchidiebere@yahoo.com>
2019-05-22 17:57:33 -0400

    recurrence

---
+++ Recursion.java
@@ -0,0 +1,25 @@
+
+
+// assignment, author - vincent, 5/21/2019
+
+public class Recursion{
+    int x;
+    int y;
+    int factorial = 1;
+
+    public int substract(int x, int y){
+        int result = x - y;
+        return result; 
+    // using do while conditional statement
+        
+        do {    
+            x --;
+        }
+            while( x > 0); 
+
+
+      
+        }
+    
+    }
+
