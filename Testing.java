3fe710b5e2cb0881998ca7575598d44f43b5529d
okengwu <okengwuchidiebere@yahoo.com>
2019-05-22 17:54:44 -0400

    array

---
+++ Testing.java
@@ -0,0 +1,43 @@
+public class Testing{
+
+
+public static void main(String[] args){
+
+    int [] m = {1,4,7,10,6,5,8,3,2,9}; // initialize list specifies each value of an element
+    int [] fig = new int [10]; // create the array object
+
+    //  compare the elements to determine the lowest then form in ascending format
+
+    if(1 < 2){
+        System.out.println("1");
+    }
+        else if  (2 < 3)
+         System.out.println("2");
+         else if (3 < 4)
+         System.out.println("3");
+
+         else if (4 < 5)
+         System.out.println("4");
+
+         else if (5 < 6)
+         System.out.println("5");
+
+         else if (6 < 7)
+         System.out.println("6");
+
+         else if (7 < 8)
+         System.out.println("7");
+
+         else if(8 < 9)
+         System.out.println("8");
+         
+         else if (9 < 10)
+         System.out.println("9");
+         else if (9 > 10)
+         System.out.println();
+         else 
+         System.out.println("10");
+         
+    
+}
+}
